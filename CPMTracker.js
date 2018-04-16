1c8a8a12df611f30b6dc9b43a16988e392f358d9
Eric King <cg100146@comporium.com>
2018-04-16 14:38:15 -0400

    updated comments

--- CPMTracker.js
+++ CPMTracker.js
@@ -9,11 +9,11 @@
 // setCpm()
 // Sets the Cpm bid for this ad group
 // Please note that although this method will effectively set the Cpm bid for this
-// ad groun, the change may have no effect on actual bidding if this ad group
+// ad group, the change may have no effect on actual bidding if this ad group
 // has a bidding strategy which does not involve cpm bids.
+
 // CPM Tracking Script
 
-// Implement 50+ workaround eventually
 // Select the account
 // Get stats for last 14 days
 // Grab Avg. Cpm
@@ -60,10 +60,10 @@ function main() {
     if(currentBiddingStrategy === "MANUAL_CPM") {
         if(currentCpm >= cpmCheck && currentCpm < cpmAboveAverage) {
             Logger.log("CPM is greater than $1.25 and under the max");
-            // setCpm() - adGroup.setCpm(bidDownOnce);
+            setCpm() - adGroup.setCpm(bidDownOnce);
         }   else if(currentCpm > cpmAboveAverage && currentCpm < maxCpm) {
             Logger.log("CPM is above average - bidding down by $1.00");
-            // setCpm() - adGroup.setCpm(bidDownTwice);
+            setCpm() - adGroup.setCpm(bidDownTwice);
         }
             else if(currentCpm > maxCpm) {
             // NOTIFY
